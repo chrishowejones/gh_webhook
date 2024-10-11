83e5d61325376d879c517a11682b4cf779a24f08
Chris Howe-Jones <chris.howejones@gmail.com>
2024-10-11 17:35:30 +0100

    Update console.log to show deployment_status

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // You installed the `express` library earlier. For more information, see "[JavaScript example: Install dependencies](#javascript-example-install-dependencies)."
-const express = require('express');
+const express = require("express");
 
 // This initializes a new Express application.
 const app = express();
@@ -8,15 +8,15 @@ const app = express();
 //
 // Once you deploy your code to a server and update your webhook URL, you should change this to match the path portion of the URL for your webhook.
 app.post(
-  '/webhook',
-  express.json({ type: 'application/json' }),
+  "/webhook",
+  express.json({ type: "application/json" }),
   (request, response) => {
     // Respond to indicate that the delivery was successfully received.
     // Your server should respond with a 2XX response within 10 seconds of receiving a webhook delivery. If your server takes longer than that to respond, then GitHub terminates the connection and considers the delivery a failure.
-    response.status(202).send('Accepted');
+    response.status(202).send("Accepted");
 
     // Check the `x-github-event` header to learn what event type was sent.
-    const githubEvent = request.headers['x-github-event'];
+    const githubEvent = request.headers["x-github-event"];
 
     // You should add logic to handle each event type that your webhook is subscribed to.
     // For example, this code handles the `issues` and `ping` events.
@@ -25,48 +25,48 @@ app.post(
     // For example, this code handles the `opened` and `closed` actions for the `issue` event.
     //
     // For more information about the data that you can expect for each event type, see "[AUTOTITLE](/webhooks/webhook-events-and-payloads)."
-    if (githubEvent === 'issues') {
+    if (githubEvent === "issues") {
       const data = request.body;
       const action = data.action;
-      if (action === 'opened') {
+      if (action === "opened") {
         console.log(`An issue was opened with this title: ${data.issue.title}`);
-      } else if (action === 'closed') {
+      } else if (action === "closed") {
         console.log(`An issue was closed by ${data.issue.user.login}`);
       } else {
         console.log(`Unhandled action for the issue event: ${action}`);
       }
-    } else if (githubEvent === 'workflow_run') {
+    } else if (githubEvent === "workflow_run") {
       const data = request.body;
       const action = data.action;
       console.log(
-        `A workflow_run fired with this name ${data.workflow_run.name} with action ${action}`
+        `A workflow_run fired with this name ${data.workflow_run.name} with action ${action}`,
       );
-    } else if (githubEvent === 'workflow_job') {
+    } else if (githubEvent === "workflow_job") {
       const data = request.body;
       const action = data.action;
       console.log(
-        `A workflow_job fired with this name ${data.workflow_job.workflow_name} with action ${action}`
+        `A workflow_job fired with this name ${data.workflow_job.workflow_name} with action ${action}`,
       );
-    } else if (githubEvent === 'deployment') {
+    } else if (githubEvent === "deployment") {
       const data = request.body;
       const action = data.action;
       console.log(
-        `A deployment fired with this id ${data.deployment.id} with action ${action}`
-      );
-      console.log(data);
-    } else if (githubEvent === 'deployment_statuses') {
-      const data = request.body;
-      const action = data.action;
-      console.log(
-        `A workflow_job fired with this name ${data.deployment_status.id} with action ${action}`
+        `A deployment fired with this id ${data.deployment.id} with action ${action}`,
       );
       console.log(data);
-    } else if (githubEvent === 'ping') {
-      console.log('GitHub sent the ping event');
+    } else if (githubEvent === "deployment_status") {
+      const data = request.body;
+      const action = data.action;
+      console.log(
+        `A deployment_status fired with this name ${data.deployment_status.id} with action ${action}`,
+      );
+      console.log(data);
+    } else if (githubEvent === "ping") {
+      console.log("GitHub sent the ping event");
     } else {
       console.log(`Unhandled event: ${githubEvent}`);
     }
-  }
+  },
 );
 
 // This defines the port where your server should listen.
